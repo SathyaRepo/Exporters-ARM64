bcf276c9e0ca15ae309a72cf6b698c5a7e3da017
SathyaRepo <132055343+SathyaRepo@users.noreply.github.com>
2023-10-17 10:36:47 +0530

    Create SNMPSU.sh

---
+++ SNMPSU.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Update the package list
+apt update
+
+# Install necessary packages
+apt install -y snmp snmpd
+
+# Retrieve SNMP Exporter Files
+echo "Retrieving SNMP Exporter Files"
+wget https://github.com/prometheus/snmp_exporter/releases/download/v0.24.1/snmp_exporter-0.24.1.linux-arm64.tar.gz
+
+# Unpack the SNMP Exporter archive
+echo "Retrieved Package... Unpacking"
+tar xvfz snmp_exporter-0.24.1.linux-arm64.tar.gz
+
+sudo useradd --system prometheus
+
+# Change to the SNMP Exporter directory
+#cd snmp_exporter-0.29.0.linux-arm64
+
+# Copy the SNMP Exporter binary to /usr/sbin
+sudo cp snmp_exporter /usr/sbin/
+
+# Create a systemd service file for SNMP Exporter
+echo "Creating SNMP Exporter Service file"
+sudo tee /etc/systemd/system/snmp-exporter.service <<EOF
+[Unit]
+Description=Prometheus SNMP Exporter Service
+After=network.target
+
+[Service]
+Type=simple
+User=prometheus
+ExecStart=/usr/local/bin/snmp_exporter --config.file="/usr/local/bin/snmp.yml"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Save and exit from the text editor
+sudo systemctl daemon-reload
+sudo systemctl enable snmp_exporter
+sudo systemctl start snmp_exporter
